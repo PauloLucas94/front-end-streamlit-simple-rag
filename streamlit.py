80f718fce3101bdbbd67bea152bfa0dec6b54a4e
Paulo Lucas <47069426+PauloLucas94@users.noreply.github.com>
2024-12-19 20:30:34 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 # URL da sua API
-API_URL = "http://35.174.95.87" #os.getenv('MY_SERVER_IP')# IP que funcionou no curl
+API_URL = os.getenv('MY_SERVER_IP')# IP que funcionou no curl "http://35.174.95.87"
 
 st.title("Assistente da Fabrica de Cursos - Metalmecânica")
 
